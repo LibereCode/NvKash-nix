5f1d389db8aaa57f4b94a6e2ca977d098a521875
LibereCode <libere-code@posteo.org>
2026-08-22 07:36:09 +0200

    fix: lua importer

--- modules/nixvim/lua/lua.nix
+++ modules/nixvim/lua/lua.nix
@@ -22,7 +22,13 @@
 
       config =
         let
-          # lua_files = [ "mappings" "options" "autocmds" ];
+          lua_dir_files = builtins.attrNames (
+            lib.filterAttrs (n: v: lib.hasSuffix ".lua" n && v == "regular") (builtins.readDir ./.)
+          );
+          files = lib.genAttrs' lua_dir_files (
+            name: lib.nameValuePair ("lua/" + name) { extraConfigLua = builtins.readFile (./. + "/${name}"); }
+          );
+
           files_lua = builtins.filter (name: lib.hasPrefix "lua/" name) (builtins.attrNames config.files);
           files_lua_strip = map (name: lib.removeSuffix ".lua" (lib.removePrefix "lua/" name)) files_lua;
         in
@@ -33,6 +39,13 @@
             -- extraConfigLuaPost (nixvim):
             --[[
               ```nix
+              lua_dir_files = builtins.attrNames (
+                lib.filterAttrs (n: v: lib.hasSuffix ".lua" n && v == "regular") (builtins.readDir ./.)
+              );
+              files = lib.genAttrs' lua_dir_files (
+                name: lib.nameValuePair ("lua/" + name) { extraConfigLua = builtins.readFile (./. + "/$<name>"); }
+              );
+
               files_lua = builtins.filter (name: lib.hasPrefix "lua/" name) (builtins.attrNames config.files);
               files_lua_strip = map (name: lib.removeSuffix ".lua" (lib.removePrefix "lua/" name)) files_lua;
               ```
@@ -40,10 +53,11 @@
           ''
           + concatStringsSep "\n" (map (file: /* lua */ "require('${file}')") files_lua_strip);
 
+          inherit files;
           ## See: <https://nix-community.github.io/nixvim/NeovimOptions/index.html#files>
-          files = lib.genAttrs' [ "mappings" "options" "autocmds" ] (
-            file: lib.nameValuePair "lua/${file}.lua" { extraConfigLua = readFile (./. + "/${file}.lua"); }
-          );
+          # files = lib.genAttrs' [ "mappings" "options" "autocmds" ] (
+          #   file: lib.nameValuePair "lua/${file}.lua" { extraConfigLua = readFile (./. + "/${file}.lua"); }
+          # );
 
           ## See: <https://nix-community.github.io/nixvim/NeovimOptions/extraFiles/index.html#extrafiles>
           ## extraFiles.<file>.text <=> files.<file>.extraConfigLua
