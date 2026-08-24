7adf4c1f2ba1ab421eb39570ad1a24fc65610808
LibereCode <libere-code@posteo.org>
2026-08-24 02:31:27 +0200

    fix: nix indentexpr off -> nix cindent

--- modules/nixvim/plugins/treesitter.nix
+++ modules/nixvim/plugins/treesitter.nix
@@ -4,7 +4,12 @@ let
 in
 {
   flake.nixvimModules.${plugin_name} =
-    { pkgs, lib, ... }@a:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }@a:
     let
       inherit (lib.nixvim) mkraw;
     in
@@ -14,79 +19,59 @@ in
         ${plugin_name} = {
           enable = true;
           highlight.enable = true;
-          indent.enable = true;
           folding.enable = true;
+          indent = {
+            enable = true;
+            # disable = [
+            #   ## disable treesitter folding for these ft:
+            #   "nix" # it works kinda shit for nix
+            # ];
+            ## Either a list of a lua-function.
+            ## See: <https://nix-community.github.io/nixvim/plugins/treesitter/indent.html#pluginstreesitterindentdisable>
+            disable.__raw = ''
+              ---Values recieved from nixvim? Via tree-sitter?
+              ---@param lang string tree-sitter language name
+              ---@param buf integer `bufnr` of target
+              ---@param ft string `filetype` of target
+              ---@return boolean Disable when returning `true`
+              function(lang, buf, ft)
+                if lang == "nix" then
+                  vim.bo[buf].cindent = true
+                  vim.cmd([[setlocal cinwords+=let]]) -- why? sucks ass
+                  -- vim.bo[buf].cinkeys = ""                  return true
+                end
+              end
+            '';
+          };
 
-          # grammarPackages = with args.config.programs.nixvim.plugins.treesitter.package; [
-          grammarPackages = with a.config.plugins.treesitter.package.builtGrammars; [
-            awk
-            bash
-            c
-            c_sharp
-            clojure
-            cmake
-            comment
-            commonlisp
-            cpp
-            css
-            diff
-            dockerfile
-            editorconfig
-            fish
-            git_config
-            git_rebase
-            gitattributes
-            gitcommit
-            gitignore
-            go
-            gpg
-            html
-            hyprlang
-            ini
-            java
-            javadoc
-            javascript
-            jq
-            jsdoc
-            json
-            json5
-            kdl
-            kitty
-            latex
-            lua
-            luadoc
-            make
-            markdown
-            markdown_inline
-            meson
-            nix
-            nu
-            powershell
-            printf
-            python
-            regex
-            rst
-            rust
-            scheme
-            ssh_config
-            sway
-            # tmux # removed in unstable
-            todotxt
-            toml
-            typescript
-            vim
-            vimdoc
-            xcompose
-            xml
-            xresources
-            yaml
-            yuck
-            # zathurarc # removed in unstable
-            zig
-            ziggy
-            ziggy_schema
-            zsh
-          ];
+          #NOTE: by default all grammar is installed
+          # grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+          #   awk
+          #   bash
+          #   c c_sharp clojure cmake comment commonlisp cpp css
+          #   diff dockerfile
+          #   editorconfig
+          #   fish
+          #   git_config git_rebase gitattributes gitcommit gitignore go gpg
+          #   html hyprlang
+          #   ini
+          #   java javadoc javascript jq jsdoc json json5
+          #   kdl kitty
+          #   latex lua luadoc
+          #   make markdown markdown_inline meson
+          #   nix
+          #   nu
+          #   powershell printf python
+          #   regex rst rust
+          #   scheme ssh_config sway
+          #   # tmux # removed in unstable
+          #   todotxt toml typescript
+          #   vim vimdoc
+          #   xcompose xml xresources
+          #   yaml yuck
+          #   # zathurarc # removed in unstable
+          #   zig ziggy ziggy_schema zsh
+          # ];
         };
 
         treesitter-context = {
